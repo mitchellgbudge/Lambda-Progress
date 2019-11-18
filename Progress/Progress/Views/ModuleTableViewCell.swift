1740cee74f8ecbbc9353583faa1c246762e6ef30
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-18 12:15:34 -0700

    Create custom module cell, base UI skeleton completed

---
+++ Progress/Progress/Views/ModuleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ModuleTableViewCell.swift
+//  Progress
+//
+//  Created by Mitchell Budge on 11/18/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ModuleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
