b5ee3a091566bc34cd351e1eecc13b5c49e4c83b
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-18 12:11:50 -0700

    Create custom objective cell

---
+++ Progress/Progress/Views/ObjectiveTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ObjectiveTableViewCell.swift
+//  Progress
+//
+//  Created by Mitchell Budge on 11/18/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ObjectiveTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
