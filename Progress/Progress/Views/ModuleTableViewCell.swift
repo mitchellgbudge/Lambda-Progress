24d8588400cdb8a014cc1fe0ce62499f71280466
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-18 14:00:18 -0700

    Add custom cell properties and create outlets

--- Progress/Progress/Views/ModuleTableViewCell.swift
+++ Progress/Progress/Views/ModuleTableViewCell.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class ModuleTableViewCell: UITableViewCell {
 
+    // MARK: - Outlets
+    @IBOutlet weak var moduleNameLabel: UILabel!
+    @IBOutlet weak var masteryPercentageLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
