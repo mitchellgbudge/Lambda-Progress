7c28b6d01d610b1316bb67fe56931108171448ee
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-21 10:01:14 -0700

    Edit placeholder text in progress testing files

--- Progress/ProgressTests/ProgressTests.swift
+++ Progress/ProgressTests/ProgressTests.swift
@@ -7,28 +7,28 @@
 //
 
 import XCTest
-@testable import Progress
+@testable import LambdaProgress
 
 class ProgressTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    // 1
+    
+    // 2
+    
+    // 3
+    
+    // 4
+    
+    // 5
+    
+    // 6
+    
+    // 7
+    
+    // 8
+    
+    // 9
+    
+    // 10
+    
 }
