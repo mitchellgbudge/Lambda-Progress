bd946f7ffc2ea129b4b605d5c5f52fbdc2c181b3
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-21 21:20:01 -0700

    Create and link collection view cell and outlets

---
+++ Progress/Progress/Views/CareerCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CareerCollectionViewCell.swift
+//  Progress
+//
+//  Created by Mitchell Budge on 11/21/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class CareerCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var careerLabel: UILabel!
+    
+}
